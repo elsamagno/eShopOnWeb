cf5d69fbe18c15d9d099544ce56655beb808cd97
elsamagno <elsamagno@gmail.com>
2020-01-30 17:35:41 +0000

    Change CatalogFilterPaginatedSpecificaion.cs

--- src/ApplicationCore/Specifications/CatalogFilterPaginatedSpecification.cs
+++ src/ApplicationCore/Specifications/CatalogFilterPaginatedSpecification.cs
@@ -4,7 +4,9 @@ namespace Microsoft.eShopWeb.ApplicationCore.Specifications
 {
     public class CatalogFilterPaginatedSpecification : BaseSpecification<CatalogItem>
     {
-        public CatalogFilterPaginatedSpecification(int skip, int take, string searchText, int? brandId, int? typeId)
+        public CatalogFilterPaginatedSpecification(
+            int skip, int take, 
+            string searchText, int? brandId, int? typeId)
             : base(i => 
                 (!brandId.HasValue || i.CatalogBrandId == brandId) &&
                 (!typeId.HasValue || i.CatalogTypeId == typeId) &&
