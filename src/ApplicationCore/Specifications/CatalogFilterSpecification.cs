c30ad418c443a0b2fa1e644004f8e5aa3bdb0651
elsamagno <elsamagno@gmail.com>
2020-01-31 10:40:19 +0000

    Searchbyname2

--- src/ApplicationCore/Specifications/CatalogFilterSpecification.cs
+++ src/ApplicationCore/Specifications/CatalogFilterSpecification.cs
@@ -1,16 +1,20 @@
 ﻿using Microsoft.eShopWeb.ApplicationCore.Entities;
-
+using System;
+using System.Linq.Expressions;
 namespace Microsoft.eShopWeb.ApplicationCore.Specifications
 {
 
     public class CatalogFilterSpecification : BaseSpecification<CatalogItem>
     {
+        public static Expression<Func<CatalogItem, bool>> BuildCatalogFilterExpression(
+            string searchText, int? brandId, int? typeId) {
+                return catalogItem =>
+                    (!brandId.HasValue || catalogItem.CatalogBrandId == brandId) &&
+                    (!typeId.HasValue || catalogItem.CatalogTypeId == typeId)  &&
+                    (string.IsNullOrEmpty(searchText) || catalogItem.Name.Contains(searchText, System.StringComparison.OrdinalIgnoreCase));
+            }
         public CatalogFilterSpecification(string searchText, int? brandId, int? typeId)
-            : base(catalogItem => 
-                (!brandId.HasValue || catalogItem.CatalogBrandId == brandId) &&
-                (!typeId.HasValue || catalogItem.CatalogTypeId == typeId)  &&
-                (string.IsNullOrEmpty(searchText) || catalogItem.Name.Contains(searchText))
-            )
+            : base(BuildCatalogFilterExpression(searchText, brandId, typeId))
         {
         }
     }
